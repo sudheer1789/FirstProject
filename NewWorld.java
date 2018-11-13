a96765b4a99487ee835fd84c680bab111906ff19
Sudheer1789 <sudheer1789@gmail.com>
2018-11-13 15:42:52 +0100

    This is the second modification

--- NewWorld.java
+++ NewWorld.java
@@ -3,5 +3,7 @@ class NewWorld
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to this world");
+		System.out.println("This is teh first java program");
+		System.out.println("This will follow pleanty");
 	}
 }
