2ceffddda257995f42ee9dbc61ed1ae1302c533e
sreddyc <31922925+sudheer1789@users.noreply.github.com>
2019-03-27 00:02:01 +0100

    Delete NewWorld.java

--- NewWorld.java
+++
@@ -1,9 +0,0 @@
-class NewWorld
-{
-	public static void main(String args[])
-	{
-		System.out.println("Welcome to this world");
-		System.out.println("This is teh first java program");
-		System.out.println("This will follow pleanty");
-	}
-}
